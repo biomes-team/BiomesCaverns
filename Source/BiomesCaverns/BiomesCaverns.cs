c692d3df7861a39ad091c25434c3364074206e51
zyllle <zyll3on@gmail.com>
2020-03-11 12:11:18 -0600

    Empty 1.1 setup

---
+++ Source/BiomesCaverns/BiomesCaverns.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+
+namespace BiomesCaverns
+{
+    [StaticConstructorOnStartup]
+    public static class BiomesCaverns
+    {
+        public const string Id = "rimworld.biomes.caverns";
+        
+        static BiomesCaverns()
+        {
+            new Harmony(Id).PatchAll();
+            Log.Message("Biomes! Caverns initialized");
+        }
+
+
+    }
+}
--- Source/BiomesCaverns/Properties/AssemblyInfo.cs
+++ Source/BiomesCaverns/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BiomesCaverns")]
-[assembly: AssemblyCopyright("Copyright © The Biomes Team 2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d6413c84-8091-4b92-b131-2aa40dcae2f3")]
+[assembly: Guid("f6f666fe-8891-4bbf-be7f-ae5b1a6d1d37")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-beta")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
