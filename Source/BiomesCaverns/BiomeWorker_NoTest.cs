38a539fe33ed9b62f00871570069f443171ecbd0
Zylleon <56230821+Zylleon@users.noreply.github.com>
2021-09-02 19:34:58 -0600

    Tunnels cave map gen

---
+++ Source/BiomesCaverns/BiomeWorker_NoTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using RimWorld.Planet;
+
+namespace BiomesCaverns
+{
+    public class BiomeWorker_NoTest : BiomeWorker
+    {
+        public override float GetScore(Tile tile, int tileID)
+        {
+
+            return -100;
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
