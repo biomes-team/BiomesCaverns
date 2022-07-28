88b9cd70e770bb4a41f935c92f92ce80fc8a6bf6
Taranchuk <nfhfyxer@gmail.com>
2022-07-28 19:31:26 +0300

    update

---
+++ 1.3/Source/BiomesCaverns/ActiveDrillPod.cs
@@ -0,0 +1,26 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BiomesCaverns
+{
+    [StaticConstructorOnStartup]
+    public class ActiveDrillPod : ActiveDropPod
+    {
+        public override void Tick()
+        {
+            if (contents == null)
+            {
+                return;
+            }
+            contents.innerContainer.ThingOwnerTick();
+            if (base.Spawned)
+            {
+                age++;
+                if (age > contents.openDelay)
+                {
+                    PodOpen();
+                }
+            }
+        }
+    }
+}
---
+++ 1.3/Source/BiomesCaverns/BC_DefOf.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BiomesCaverns
+{
+    [DefOf]
+    public static class BC_DefOf
+    {
+        public static ThingDef BMT_DrillPodLeaving;
+        public static ThingDef BMT_DrillPodIncoming;
+        public static ThingDef BMT_DrillPodActive;
+        public static ThingDef BMT_DrillPod;
+        public static WorldObjectDef BMT_TravelingDrillPods;
+    }
+}
