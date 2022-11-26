c658d838d3c5579ad3d9feb8c089e9e785aa9a6c
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2022-11-26 08:53:02 +0300

    EarthenDepths Generators
    Added support for stalagmites for EarthenDepths. As well as a special generator of red crystals.
    
    I also threw the sauces of my shit code

---
+++ Source/BMT_ScatterableCrystals/GenStep_ScatterCrystals.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace BMT
+{
+    class GenStep_ScatterCrystals : GenStep_ScatterGroup
+	{
+		protected virtual bool ShouldSkipMapCrystal(Map map)
+		{
+			if (map.Biome.defName.Contains("Crystal"))
+			{
+				return false;
+			}
+			return true;
+		}
+		public override void Generate(Map map, GenStepParams parms)
+		{
+			if (ShouldSkipMapCrystal(map))
+			{
+				return;
+			}
+			else if (ShouldSkipMap(map))
+			{
+				return;
+			}
+			int num = CalculateFinalCount(map);
+			for (int i = 0; i < num; i++)
+			{
+				if (!TryFindScatterCell(map, out var result))
+				{
+					return;
+				}
+				ScatterAt(result, map, parms);
+				usedSpots.Add(result);
+			}
+			usedSpots.Clear();
+		}
+	}
+	class GenStep_ScatterRedCrystals : GenStep_ScatterGroup
+	{
+		protected virtual bool ShouldSkipMapCrystal(Map map)
+		{
+			if (map.Biome.defName.Contains("BMT_EarthenDepths"))
+			{
+				return false;
+			}
+			return true;
+		}
+		public override void Generate(Map map, GenStepParams parms)
+		{
+			if (ShouldSkipMapCrystal(map))
+			{
+				return;
+			}
+			else if (ShouldSkipMap(map))
+			{
+				return;
+			}
+			int num = CalculateFinalCount(map);
+			for (int i = 0; i < num; i++)
+			{
+				if (!TryFindScatterCell(map, out var result))
+				{
+					return;
+				}
+				ScatterAt(result, map, parms);
+				usedSpots.Add(result);
+			}
+			usedSpots.Clear();
+		}
+	}
+}
