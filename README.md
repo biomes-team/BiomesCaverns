122d15cf831ff4a6e6919a5ed0bda6922182f8a4
XidonEye <77588639+XidonEye@users.noreply.github.com>
2023-05-01 08:56:00 +0200

    release

--- README.md
+++
@@ -1,9 +0,0 @@
-# Biomes! Caverns
-
-The caverns module for the Biomes! mods. Requires Biomes! Core loaded before it.
-
-## Features
-
-## Known Issues
-
-## Known Mod Conflicts
--- Source/BiomesCaveworldFlora/Util_Caveworld_Flora_Unleashed.cs
+++
@@ -1,9 +0,0 @@
-﻿using Verse;
-
-public static class Util_Caveworld_Flora_Unleashed
-{
-    public static ThingDef fungiponicsBasinDef => ThingDef.Named("BMT_FungiponicsBasin");
-    public static ThingDef MyceliumDef => ThingDef.Named("BMT_Mycelium");
-    public static ThingDef MoteGleamcapSmokeDef => ThingDef.Named("Mote_GleamcapSmoke");
-    public static HediffDef gleamcapSmokeDef => HediffDef.Named("BMT_HediffGleamcapSmoke");
-}
