d395531915366535a979a2c57c6071f32667af13
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2024-03-14 00:11:54 +0100

    Update to the new version of Biomes! Core.

--- Source/BiomesCaverns/Patches/DropCellFinder_RandomDropSpot_Patch.cs
+++ Source/BiomesCaverns/Patches/DropCellFinder_RandomDropSpot_Patch.cs
@@ -2,19 +2,21 @@
 using RimWorld;
 using Verse;
 
-namespace BiomesCaverns
+namespace BiomesCaverns.Patches
 {
-    [HarmonyPatch(typeof(DropCellFinder), "RandomDropSpot")]
-    public static class DropCellFinder_RandomDropSpot_Patch
-    {
-        public static bool Prefix(ref IntVec3 __result, Map map, bool standableOnly = true)
-        {
-            if (TravelingTransportPods_Arrived_Patch.drillPodIncoming)
-            {
-                __result = CellFinderLoose.RandomCellWith((IntVec3 c) => (!standableOnly || c.Standable(map)) && !c.Fogged(map), map);
-                return false;
-            }
-            return true;
-        }
-    }
+	[HarmonyPatch(typeof(DropCellFinder), "RandomDropSpot")]
+	public static class DropCellFinder_RandomDropSpot_Patch
+	{
+		public static bool Prefix(ref IntVec3 __result, Map map, bool standableOnly = true)
+		{
+			if (TravelingTransportPods_Arrived_Patch.drillPodIncoming)
+			{
+				__result = CellFinderLoose.RandomCellWith((IntVec3 c) => (!standableOnly || c.Standable(map)) && !c.Fogged(map),
+					map);
+				return false;
+			}
+
+			return true;
+		}
+	}
 }
--- Source/BiomesCaverns/Patches/TravelingTransportPods_Arrived_Patch.cs
+++ Source/BiomesCaverns/Patches/TravelingTransportPods_Arrived_Patch.cs
@@ -1,22 +1,24 @@
 ﻿using HarmonyLib;
 using RimWorld.Planet;
 
-namespace BiomesCaverns
+namespace BiomesCaverns.Patches
 {
-    [HarmonyPatch(typeof(TravelingTransportPods), "Arrived")]
-    public static class TravelingTransportPods_Arrived_Patch
-    {
-        public static bool drillPodIncoming;
-        public static void Prefix(TravelingTransportPods __instance)
-        {
-            if (__instance.def == BC_DefOf.BMT_TravelingDrillPods)
-            {
-                drillPodIncoming = true;
-            }
-        }
-        public static void Postfix()
-        {
-            drillPodIncoming = false;
-        }
-    }
+	[HarmonyPatch(typeof(TravelingTransportPods), "Arrived")]
+	public static class TravelingTransportPods_Arrived_Patch
+	{
+		public static bool drillPodIncoming;
+
+		public static void Prefix(TravelingTransportPods __instance)
+		{
+			if (__instance.def == BC_DefOf.BMT_TravelingDrillPods)
+			{
+				drillPodIncoming = true;
+			}
+		}
+
+		public static void Postfix()
+		{
+			drillPodIncoming = false;
+		}
+	}
 }
