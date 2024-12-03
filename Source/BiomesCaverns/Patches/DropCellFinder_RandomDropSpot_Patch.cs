e268f75dda9cfd4dbd5cfa981b93b6c0cefdc680
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2024-12-03 16:52:57 +0300

    Anomaly compt fix
    Now the caverns should be fully compatible with the anomaly. Fleshbeast holes, noctolites and those things from the final quest should spawn normally (with the caveat that they can spawn in the player's base). Except for obelisks, since they fall from the sky, and there is no sky in the caverns :)

--- Source/BiomesCaverns/Patches/DropCellFinder_RandomDropSpot_Patch.cs
+++ Source/BiomesCaverns/Patches/DropCellFinder_RandomDropSpot_Patch.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System;
 using Verse;
 
 namespace BiomesCaverns.Patches
@@ -23,4 +24,56 @@ namespace BiomesCaverns.Patches
 			return true;
 		}
 	}
+
+    //[HarmonyPatch(typeof(CellFinderLoose), nameof(CellFinderLoose.TryFindSkyfallerCell))]
+    //public static class CellFinderLoose_TryFindSkyfallerCell_Patch
+    //{
+    //	public static bool Prefix(ref bool __result, ThingDef skyfaller, Map map, int minDistToEdge, ref IntVec3 cell)
+    //	{
+    //		if (map?.Biome?.GetModExtension<BiomesCore.DefModExtensions.BiomesMap>()?.isCavern == true)
+    //		{
+    //               bool validator(IntVec3 x)
+    //               {
+    //                   foreach (IntVec3 item in GenAdj.OccupiedRect(x, Rot4.North, skyfaller.size))
+    //                   {
+    //                       if (!item.InBounds(map) || item.Fogged(map) || !item.Standable(map))
+    //                       {
+    //                           return false;
+    //					}
+    //					foreach (Thing thing in item.GetThingList(map))
+    //					{
+    //						if (thing.def.preventSkyfallersLandingOn)
+    //						{
+    //							return false;
+    //						}
+    //					}
+    //				}
+    //				if (!map.reachability.CanReachColony(x))
+    //				{
+    //					return false;
+    //				}
+    //				return true;
+    //			}
+    //			__result = CellFinderLoose.TryFindRandomNotEdgeCellWith(minDistToEdge, validator, map, out cell);
+    //			return !__result;
+    //		}
+    //		return true;
+    //	}
+
+    //}
+
+    [HarmonyPatch(typeof(LargeBuildingCellFinder), nameof(LargeBuildingCellFinder.TryFindCell))]
+    public static class LargeBuildingCellFinder_TryFindCell_Patch
+    {
+        public static bool Prefix(Map map, ref LargeBuildingSpawnParms parms)
+        {
+            if (map?.Biome?.GetModExtension<BiomesCore.DefModExtensions.BiomesMap>()?.isCavern == true)
+            {
+                parms.attemptSpawnLocationType = SpawnLocationType.Anywhere;
+            }
+            return true;
+        }
+
+    }
+
 }
