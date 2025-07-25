4f5f4c14f6e4e289621817b842fb662c5628133c
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2025-07-25 12:21:01 +0300

    incidents

---
+++ Source/BiomesCaverns/Incidents/IncidentWorker_Earthquake.cs
@@ -0,0 +1,18 @@
+﻿using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace BiomesCore
+{
+
+    // Should be moved to Caverns
+    //public class IncidentWorker_Earthquake : IncidentWorker
+    //{
+    //    protected override bool TryExecuteWorker(IncidentParms parms)
+    //    {
+    //        Find.CurrentMap.gameConditionManager.RegisterCondition(new GameCondition_Earthquake(Rand.Value * 6, Mathf.RoundToInt(1 + Rand.Value * 3 * 2500)));
+    //        //SendStandardLetter(parms, null);
+    //        return true;
+    //    }
+    //}
+}
