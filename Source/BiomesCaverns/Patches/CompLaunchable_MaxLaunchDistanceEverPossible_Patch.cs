8eb99d830ef42e0dc78e3923dc03412b39c52093
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2026-04-19 16:42:24 +0300

    Drill pod crash fix

--- Source/BiomesCaverns/Patches/CompLaunchable_MaxLaunchDistanceEverPossible_Patch.cs
+++ Source/BiomesCaverns/Patches/CompLaunchable_MaxLaunchDistanceEverPossible_Patch.cs
@@ -10,20 +10,20 @@ namespace BiomesCaverns.Patches
 	/// <summary>
 	/// Inject drillpod specific behavior in CompLaunchable.
 	/// </summary>
-	[HarmonyPatch(typeof(CompLaunchable), "MaxLaunchDistanceEver")]
-	public class CompLaunchable_MaxLaunchDistanceEver_Patch
-	{
-		private static bool Prefix(CompLaunchable __instance, PlanetLayer layer, ref int __result)
-		{
-			if (__instance.parent.def != BC_DefOf.BMT_DrillPod)
-			{
-				return true;
-			}
+	//[HarmonyPatch(typeof(CompLaunchable), "MaxLaunchDistanceEver")]
+	//public class CompLaunchable_MaxLaunchDistanceEver_Patch
+	//{
+	//	private static bool Prefix(CompLaunchable __instance, PlanetLayer layer, ref int __result)
+	//	{
+	//		if (__instance.parent.def != BC_DefOf.BMT_DrillPod)
+	//		{
+	//			return true;
+	//		}
 
-			int num = __instance.MaxLaunchDistanceEver(layer);
+	//		int num = __instance.MaxLaunchDistanceEver(layer);
 
-			__result = Mathf.FloorToInt(num / (2.25f * 3f));
-			return false;
-		}
-	}
+	//		__result = Mathf.FloorToInt(num / (2.25f * 3f));
+	//		return false;
+	//	}
+	//}
 }
